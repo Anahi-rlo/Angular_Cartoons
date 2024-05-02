604fc0720c76383d643b5856a1fde2e33a524d44
Anahi-Nany <a324947@alumnos.uaslp.mx>
2024-05-01 22:33:47 -0600

    Creation of addresses, pages, error path and home page redirection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomePage } from './pages/home/home.page';
+import { ContactPage } from './pages/contact/contact.page';
+import { AboutPage } from './pages/about/about.page';
+import { TvShowsPage } from './pages/tv-shows/tv-shows.page';
+import { ErrorPage } from './pages/error/error.page';
+//llenamos de rutas que vamos a utilizar
+//path: url
+//component: a cual comonente se referencia
+export const routes: Routes = [
+    //redireccionar: el full simboliza que haga mach con la ruta
+    { path: "", redirectTo: "tvshows", pathMatch: "full" },
+    { path: "home", component: HomePage },
+    { path: "contact", component: ContactPage },
+    { path: "about", component: AboutPage },
+    { path: "tvshows", component: TvShowsPage },
+    //definimos una ruta de mensaje de error
+    { path: "error", component: ErrorPage },
+    /* ** = cualquier ruta q no esta definida  aqui va arenderizar este componente
+    en este caso la pantalla error*/
+    //esta debe de encontrarse al final, para que si no encuentra ninguna de la lista, esta las elimine
+    { path: "**", redirectTo: "error", pathMatch: "full" }
+];
